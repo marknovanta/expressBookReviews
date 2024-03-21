689dc21ed10db5b4f506ea0a80d73e44510578e8
Marco <marco@example.com>
2024-03-21 06:10:09 -0400

    Make async functions in general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -26,17 +26,17 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/', async function (req, res) {
   //Write your code here
-  const bookList = JSON.stringify(books);
+  const bookList = await JSON.stringify(books);
   return res.status(200).json({books: bookList});
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
-  const book = books[isbn]
+  const book = await books[isbn]
   
   if (book) {
     return res.status(200).json(book);
@@ -47,10 +47,10 @@ public_users.get('/isbn/:isbn',function (req, res) {
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
   //Write your code here
   const author = req.params.author;
-  const authorBooks = Object.values(books).filter(book => book.author === author)
+  const authorBooks = await Object.values(books).filter(book => book.author === author)
 
   if (authorBooks.length > 0) {
     return res.status(200).json(authorBooks)
@@ -60,10 +60,10 @@ public_users.get('/author/:author',function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+public_users.get('/title/:title', async function (req, res) {
   //Write your code here
   const title = req.params.title;
-  const titleBooks = Object.values(books).filter(book => book.title.toLowerCase() === title.toLowerCase());
+  const titleBooks = await Object.values(books).filter(book => book.title.toLowerCase() === title.toLowerCase());
   
   if (titleBooks.length > 0) {
     return res.status(200).json(titleBooks);
